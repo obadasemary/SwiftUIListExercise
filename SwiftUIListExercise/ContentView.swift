d17eae9ac9f7f08ce9a7cdf0e4aba37371f1855c
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-07 00:19:00 +0300

    Initial Commit

---
+++ SwiftUIListExercise/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIListExercise
+//
+//  Created by Abdelrahman Mohamed on 7.09.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIListExercise/SwiftUIListExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIListExerciseApp.swift
+//  SwiftUIListExercise
+//
+//  Created by Abdelrahman Mohamed on 7.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIListExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
