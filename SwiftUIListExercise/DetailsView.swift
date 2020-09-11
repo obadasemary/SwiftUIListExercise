942a659d83518da57249505891994a40c2f605a2
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-11 19:21:56 +0300

    Add Details View
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ SwiftUIListExercise/DetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailsView.swift
+//  SwiftUIListExercise
+//
+//  Created by Abdelrahman Mohamed on 11.09.2020.
+//
+
+import SwiftUI
+
+struct DetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailsView()
+    }
+}
