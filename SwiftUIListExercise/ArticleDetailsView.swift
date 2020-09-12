fafbec4b9d2ac6d2c8d295a6a25baa2ffcea8a0d
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-12 03:28:49 +0300

    An even more Elegant UI with a Custom Back Button
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>
    
    ## How to extend the scroll view to the very top of the screen
    ## How to create a custom back button and trigger the navigation programmatically

--- SwiftUIListExercise/ArticleDetailsView.swift
+++ SwiftUIListExercise/ArticleDetailsView.swift
@@ -14,17 +14,16 @@ struct ArticleDetailsView: View {
     var body: some View {
         ScrollView {
             VStack(alignment: .leading) {
-                
                 Image(article.image)
                     .resizable()
-                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-                
+                    .aspectRatio(contentMode: .fit)
+                    
                 Group {
                     Text(article.title)
                         .font(.system(.title, design: .rounded))
                         .fontWeight(.black)
                         .lineLimit(3)
-                    
+                        
                     Text("By \(article.author)".uppercased())
                         .font(.subheadline)
                         .foregroundColor(.secondary)
@@ -38,8 +37,18 @@ struct ArticleDetailsView: View {
                     .lineLimit(1000)
                     .multilineTextAlignment(.leading)
             }
-            .navigationBarTitle("", displayMode: .inline)
         }
+        .edgesIgnoringSafeArea(.top)
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading:
+                                Button(action: {
+                                    // Navigate to the previous screen
+                                }, label: {
+                                    Image(systemName: "chevron.left.circle.fill")
+                                        .font(.largeTitle)
+                                        .foregroundColor(.white)
+                                })
+        )
     }
 }
 
@@ -48,21 +57,5 @@ struct DetailsView_Previews: PreviewProvider {
         NavigationView {
             ArticleDetailsView(article: articles[0])
         }
-//        ArticleDetailsView(
-//            article:
-//                Article(
-//                    title: "Vision Framework: Working with Text and Image Recognition",
-//
-//                    author: "Sai Kambampati",
-//
-//                    rating: 4,
-//
-//                    excerpt: "2 years ago, at WWDC 2017, Apple released the Vision framework, an amazing, intuitive framework that would make it easy for developers to add computer vision to their apps.",
-//
-//                    image: "vision-framework",
-//
-//                    content: "2 years ago, at WWDC 2017, Apple released the Vision framework, an amazing, intuitive framework that would make it easy for developers to add computer vision to their apps. Everything from text detection to facial detection to barcode scanners to integration with Core ML was covered in this framework.\n\nThis year, at WWDC 2019, Apple released several more new features to this framework that really push the field of computer vision. That’s what we’ll be looking at in this tutorial.\n\nFor many years now, Snapchat has reigned as the popular social media app among teens. With its simple UI and great AR features, high schoolers around the world love to place cat/dog filters themselves. Let’s flip the script!"
-//                )
-//        )
     }
 }
